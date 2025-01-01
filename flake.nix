1d2f58c30b54edbbf98dfb6de03281c09dda8eb9
grossamos <email@amosgross.com>
2025-01-01 15:22:29 +0100

    added lock

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "adversial ai class";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.typescript
+        pkgs.typescript-language-server
+      ];
+    };
+
+  };
+}
